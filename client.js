6c5c59f55251db2a365e194bed61c76b056002fe
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-11-02 16:35:22 -0500

    pair program exercise

--- client.js
+++ client.js
@@ -39,4 +39,62 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+function giveMoneyToTheFilthyMasses (name) {
+        if (i.reviewRating === 3){
+      let compensation = i.annualSalary * 1.04;
+      let bonus = compensation - i.annualSalary;
+      let bonus3 = {
+        name: i.name,
+        bonusPercentage: 1.04,
+        totalCompensation: compensation,
+        totalBonus: bonus
+      }
+      return bonus3;
+ 
+    } else if (i.reviewRating === 4) {
+      let compensation = i.annualSalary * 1.06;
+      let bonus = compensation - i.annualSalary;
+      let bonus4 = {
+        name: i.name,
+        bonusPercentage: 1.06,
+        totalCompensation: compensation,
+        totalBonus: bonus
+      }
+      return bonus4;
+    
+    } else if (i.reviewRating === 5){
+      let compensation = i.annualSalary * 1.01;
+      let bonus = compensation - i.annualSalary;
+      let bonus5 = {
+        name: i.name,
+        bonusPercentage: 1.10,
+        totalCompensation: compensation,
+        totalBonus: bonus
+      }
+      return bonus5;
+    }
+    else {
+      let compensation = i.annualSalary * 1;
+      let bonus = compensation - i.annualSalary;
+      let bonus0 = {
+        name: i.name,
+        bonusPercentage: 1, 
+        totalCompensation: compensation,
+        totalBonus: bonus
+      }
+      return bonus0;
+      
+    }
+    
+    }for (let i of employees)
+  }
+
+
+
+
+
+
+
+console.log(giveMoneyToTheFilthyMasses('Atticus'));
+console.log(giveMoneyToTheFilthyMasses('Scout'));
+console.log(giveMoneyToTheFilthyMasses('Robert'));
